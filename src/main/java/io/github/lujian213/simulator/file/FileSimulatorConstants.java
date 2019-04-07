cb2229946684613bf8b55f0e50fb82f6d996223f
lujian213 <lujain213@msn.com>
2019-04-07 13:59:35 +0000

    add missing files

---
+++ src/main/java/io/github/lujian213/simulator/file/FileSimulatorConstants.java
@@ -0,0 +1,10 @@
+package io.github.lujian213.simulator.file;
+
+import io.github.lujian213.simulator.SimSimulatorConstants;
+
+public class FileSimulatorConstants extends SimSimulatorConstants {
+	public static final String PROP_NAME_INPUT_DIR = "simulator.file.input.dir";
+	public static final String PROP_NAME_POLL_INTERVAL = "simulator.file.poll.interval";
+
+	public static final String HEADER_NAME_FILE_NAME = "_output.file";
+}
